6b037c2b046f9e6a644d18319b0717f764711c74
daliasabirova6@gmail.com <daliasabirova6@gmail.com>
2024-12-12 15:21:08 +0300

    Добавьте файлы проекта.

---
+++ DzFromTumakov/AbstractModels/BankAccount.cs
@@ -0,0 +1,83 @@
+﻿using DzFromTumakov.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzFromTumakov.AbstractModels
+{
+    class BankAccount
+    {
+        private static int currentId = 1;
+
+
+
+        private int accountNumber;
+        private decimal balance;
+        private BankAccountType bankAccountType;
+
+        public BankAccount(decimal balance, BankAccountType bankAccountType)
+        {
+            accountNumber = currentId++;
+
+            this.balance = balance;
+            this.bankAccountType = bankAccountType;
+        }
+
+        public void ShowInfo()
+        {
+            ShowMessage($"Номер счета: {accountNumber}, Баланс: {balance}, Тип счета: {bankAccountType}");
+        }
+
+        private void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Input(int amountOfMoney)
+        {
+            if (amountOfMoney > balance)
+            {
+                ShowMessage("На счёте недостаточно средств денег");
+            }
+            else
+            {
+                balance -= amountOfMoney;
+                ShowMessage("Деньги успешно сняты!");
+            }
+        }
+
+        public void Output(int amountOfMoney)
+        {
+            balance += amountOfMoney;
+            ShowMessage("Счет успешно пополнен!");
+        }
+
+        public void Transfer(BankAccount otherAccount, decimal amount)
+        {
+            if (otherAccount == null)
+            {
+                ShowMessage("Счет для перевода не существует.");
+                return;
+            }
+
+            if (amount <= 0)
+            {
+                ShowMessage("Сумма перевода должна быть больше нуля.");
+                return;
+            } 
+
+            if (balance < amount)
+            {
+                ShowMessage("На счёте недостаточно средств для перевода.");
+                return;
+            }
+
+            balance -= amount;
+            otherAccount.balance += amount;
+
+            ShowMessage($"Переведено {amount} со счета {accountNumber} на счет {otherAccount.accountNumber}.");
+        }
+    }
+}
---
+++ DzFromTumakov/Program.cs
@@ -0,0 +1,63 @@
+﻿using DzFromTumakov.AbstractModels;
+using DzFromTumakov.Enums;
+
+
+namespace DzFromTumakov
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            /*Упражнение 8.1 В класс банковский счет, созданный в упражнениях 7.1- 7.3 добавить
+            метод, который переводит деньги с одного счета на другой. У метода два параметра: ссылка
+            на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.*/
+
+            Console.WriteLine("Упражнение 8.1");
+
+            BankAccount bankAccount = new BankAccount(100, BankAccountType.Accumulation);
+
+            bankAccount.ShowInfo();
+
+            BankAccount bankAccount1 = new BankAccount(120, BankAccountType.Savings);
+            
+            bankAccount1.ShowInfo();
+
+            bankAccount.Transfer(bankAccount1, 50);
+
+            bankAccount.ShowInfo();
+
+            bankAccount1.ShowInfo();
+            
+        }
+        static void Task2()
+        {
+            /*Упражнение 8.2 Реализовать метод, который в качестве входного параметра принимает
+            строку string, возвращает строку типа string, буквы в которой идут в обратном порядке.
+            Протестировать метод.*/
+
+            Console.WriteLine("Упражнение 8.2");
+
+            string input = "Hello, World!";
+            string reversed = ReverseString(input);
+            Console.WriteLine($"Original: {input}");
+            Console.WriteLine($"Reversed: {reversed}");
+        }
+
+        static void Task3()
+        {
+            /*Упражнение 8.3 Написать программу, которая спрашивает у пользователя имя файла. Если
+            такого файла не существует, то программа выдает пользователю сообщение и заканчивает
+            работу, иначе в выходной файл записывается содержимое исходного файла, но заглавными
+            буквами.*/
+
+            Console.WriteLine("Упражнение 8.3");
+
+
+        }
+
+        static void Main()
+        {
+            Task1();
+        }
+    }
+}
